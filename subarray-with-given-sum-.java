09a231a3707e8b4328f23370adc8659a6d831d5c
MONICA SINGH <Monica.Singh@natwest.com>
2024-11-03 23:25:16 +0530

    anagram,string

--- subarray-with-given-sum-.java
+++ subarray-with-given-sum-.java
@@ -1,39 +1,60 @@
-https://www.geeksforgeeks.org/problems/subarray-with-given-sum-1587115621/11
+class Solution {
+    // Function is to check whether two strings are anagram of each other or not.
+    public static boolean areAnagrams(String s1, String s2) {
 
-//User function Template for Java
+        // Your code here
+        char arr[]=s1.toCharArray();
+        Arrays.sort(arr);
+         char arr2[]=s2.toCharArray();
+         boolean bl=false;
+         Arrays.sort(arr2);
+         if(s1.length()!=s2.length()){
+         return false;}
+         else{
+         for(int i=0;i<s1.length();i++){
+             if(arr[i]!=arr2[i])
+             {
+                  return false;
+                  
+             }
+             bl=true;
+        
+             
+         }}
+       if(bl)
+        return true;
+        else
+        return false;
+    }
+}
+
+//optimized
 
 class Solution {
-    static ArrayList<Integer> subarraySum(int[] arr, int target) {
-        // code here
-        int sum=0;
-        boolean bl=false;
-        ArrayList<Integer> arrr=new ArrayList<Integer>();
-        for(int i=0;i<arr.length;i++)
-        {
-         long currsum=arr[i]; 
-         if(currsum==target){
-             arrr.add(i+1);
-             arrr.add(i+1);
-              bl=true;
-             break;
-         }
-         else{
-             for(int j=i+1;j<arr.length;j++){
-                 currsum += arr[j];
-                 if(currsum==target){
-                     arrr.add(i+1);
-                     arrr.add(j+1);
-                     bl=true;
-                     break;
-                 }
-             }
-         }
-         if(bl)
-         break;
+    // Function is to check whether two strings are anagram of each other or not.
+    public static boolean areAnagrams(String s1, String s2) {
+
+        // Your code here
+        int h1[]=new int[26];
+        Arrays.fill(h1,0);
+        for(int i=0;i<s1.length();i++){
+            h1[s1.charAt(i)-'a']++;
         }
-        if (bl)
-        return arrr;
-        else{
-          arrr.add(-1);
-          return arrr;
-}}}
+         for(int i=0;i<s2.length();i++){
+            h1[s2.charAt(i)-'a']--;
+        }
+         for(int i=0;i<h1.length;i++){
+           if( h1[i]!=0)
+           {
+               return false;
+           }
+        }
+        return true;
+    }
+}
+
+
+
+
+
+

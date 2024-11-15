f81240942898a6bc46b21d95b2ba65828a3d305b
MONICA SINGH <Monica.Singh@natwest.com>
2024-11-15 22:47:01 +0530

    array

--- product_array_puzzle.java
+++ product_array_puzzle.java
@@ -1,47 +1,33 @@
-c:\Users\Lenovo\GFG\transpose - Copy (2).java
-Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
+// User function Template for Java
 
- 
-
-Example 1:
-
-Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
-Output: 8
-Explanation: There are 8 negatives number in the matrix.
 class Solution {
-    public int countNegatives(int[][] grid) {
-        int m=grid.length;
-        int count=0;
-int n=grid[0].length;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]<0){
-                    count++;
-                }
-            }
+    public static long[] productExceptSelf(int arr[]) {
+        
+       int len=arr.length;
+        long[] leftarr=new long[len];
+     if (len == 1) {
+            leftarr[0] = 1;
+            return leftarr;
         }
-        return count;
-    }
-}
-
-//opimized
-class Solution {
-    public int countNegatives(int[][] grid) {
-        int rows=grid.length;
-        int count=0;
-int cols=grid[0].length-1;
-int row=0;
-int col=0;
-while(row<rows && cols>=0){
-if(grid[row][cols]>=0){
-    row++;
-}
-else{
-    count+=rows-row;
-    cols--;
-}
-}
-return count;
-
+       long left=1;
+      
+      
+        for (int i = 0; i < len; i++) leftarr[i] = 1;
+      
+       for(int i=0;i<len;i++){
+           leftarr[i]=left;
+           left*=arr[i];
+           
+       }
+       left=1;
+       for(int i=len-1;i>=0;i--){
+           leftarr[i]*=left;
+           left*=arr[i];
+           
+       }
+      
+      return leftarr;
+    
+        // code here
     }
 }
